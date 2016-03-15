f7d56cadbbcc8aa8b599d183d5822de0369a51a0
Gerardo Martinez <gmart_nez@yahoo.com.mx>
2016-03-15 16:39:42 -0600

    C# Basics Module 1

---
+++ DinerMax3000Console/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinerMax3000Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FoodMenu summerMenu = new FoodMenu();
+            summerMenu.Name = "Summer menu";
+            summerMenu.AddMenuItem("Salmon", "Fresh Norwegian Salmon with Sandefjord butter.", 25.50);
+            summerMenu.AddMenuItem("Taco", "All Norwegians eat taco on Fridays", 10);
+            summerMenu.HospitalDirections = "Right around the corner of 5th street. Ask for Dr. Jones";
+
+            DrinkMenu outsideDrinks = new DrinkMenu();
+            outsideDrinks.Disclaimer = "Do not drink and code.";
+            outsideDrinks.AddMenuItem("Virgin Cuba Libre", "A classic.", 10);
+            outsideDrinks.AddMenuItem("Screwdriver", "Makes you hammered.", 15);
+
+            Order hungryGuestOrder = new Order();
+
+            foreach (var item in summerMenu.items)
+            {
+                hungryGuestOrder.items.Add(item);
+            }
+
+            foreach (var item in outsideDrinks.items)
+            {
+                hungryGuestOrder.items.Add(item);
+            }
+
+            Console.WriteLine($"The total is: {hungryGuestOrder.Total}");
+
+            try
+            {
+                outsideDrinks.AddMenuItem("Himkok", "9 out 10 people recommend staying away from this drink.", 0);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
